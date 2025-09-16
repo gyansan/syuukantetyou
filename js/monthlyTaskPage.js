b7bd740a9da808bf03697d97ae6b4a5ea772d7ea
gyansan <gyansan24@outlook.jp>
2025-09-17 01:33:48 +0900

    繰り返しタスクをつけた

---
+++ js/monthlyTaskPage.js
@@ -0,0 +1,59 @@
+const taskRowTemplate = document.getElementById("task-row-template");
+const taskTableBody = document.getElementById("task-tbody");
+
+//テーブルを表示する
+window.addEventListener("DOMContentLoaded", async ()=>{
+  refreshTasks();
+});
+
+async function refreshTasks() {
+  const tasks = await getAllItems("monthlyTasks");
+  tasks.sort((a,b)=> a.endTime.localeCompare(b.endTime));
+  tasks.sort((a,b)=> a.startTime.localeCompare(b.startTime));
+  tasks.sort((a,b)=> a.deadline.localeCompare(b.deadline));
+  tasks.sort((a,b)=> a.startDay.localeCompare(b.startDay));
+  renderDailyTaskTable(tasks);
+}
+
+function renderDailyTaskTable(tasks){
+  taskTableBody.innerHTML = "";
+  const emptyMessage = document.getElementById("task-empty-message");
+  if (!tasks.length) {
+    emptyMessage.style.display = "block";
+    return;
+  }
+  emptyMessage.style.display = "none";
+  tasks.forEach(task => taskTableBody.appendChild(createTaskRow(task)));
+}
+
+function createTaskRow(task) {
+  const taskRow = taskRowTemplate.content.firstElementChild.cloneNode(true);
+  taskRow.dataset.id = task.id;
+
+  const taskCell = taskRow.querySelector(".cell-task");
+  taskCell.textContent = task.title || "";
+  if (task.notes) taskCell.setAttribute("data-note", task.notes);
+
+  taskRow.querySelector(".cell-startday").textContent = task.startDay + "日" || "";
+  taskRow.querySelector(".cell-start").textContent = task.startTime || "";
+  taskRow.querySelector(".cell-end").textContent = task.endTime || "";
+  taskRow.querySelector(".cell-deadline").textContent = task.deadline + "日" || "";
+
+  return taskRow;
+}
+
+//削除・編集ボタンにイベントをつける
+// tbodyにイベントをまとめて仕込む
+taskTableBody.addEventListener("click", (e) => {
+  // 編集ボタンが押された場合
+  if (e.target.matches('[data-action="edit"]')) {
+    const row = e.target.closest("tr"); 
+    openUpdateModal(row.dataset.id, "monthly")
+  }
+  // 削除ボタンが押された場合
+  if (e.target.matches('[data-action="delete"]')) {
+    const row = e.target.closest("tr");
+    deleteItem(row.dataset.id, "monthlyTasks");
+    refreshTasks();
+  }
+});
